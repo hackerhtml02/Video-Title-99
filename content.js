9c8128a9bf661464e7fb2ef0fced75ee2ce44959
hackerhtml02 <97086315+hackerhtml02@users.noreply.github.com>
2024-02-20 00:26:34 +0500

    Update content.js

--- content.js
+++ content.js
@@ -1,10 +1,15 @@
-// Find the element by its XPath
-var xpath = "//a[@title='YouTube Automatically Watchtime Software v6.0 Full Automatic Login + Switcher.....']";
-var element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+var xpath = "//a[@title='YouTube Full Automatic Watchtime Software | Autoplay Bot Play Videos and Auto Mute & Loop in 10min']";
+var timeout = 5000; // 5 seconds in milliseconds
 
-// Click on the element
-if (element) {
-    element.click();
-} else {
-    console.error("Element not found with XPath: " + xpath);
+// Define a function to perform the XPath search and click operation
+function performClick() {
+    var element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    if (element) {
+        element.click();
+    } else {
+        console.error("Element not found with XPath: " + xpath);
+    }
 }
+
+// Set the timeout to call the function after 5 seconds
+setTimeout(performClick, timeout);
